c972abe5286a3921f2b701aef190c3664b7d25a4
JianAn Chen <john.data.chen@gmail.com>
2025-07-25 12:40:50 +0800

    feat: add config module to read .env

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,16 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./user/user.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 讓 ConfigModule 全域可用
+    }),
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
