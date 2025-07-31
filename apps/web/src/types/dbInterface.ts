edc75d380a3d7ddad9a7fd801a973995133db51e
JianAn Chen <john.data.chen@gmail.com>
2025-07-31 14:45:38 +0800

    refactor: remove unused imports

--- apps/web/src/types/dbInterface.ts
+++ apps/web/src/types/dbInterface.ts
@@ -1,9 +1,3 @@
-import { BoardModel } from '@/models/board.model';
-import { ProjectModel } from '@/models/project.model';
-import { TaskModel } from '@/models/task.model';
-import { UserModel } from '@/models/user.model';
-import mongoose from 'mongoose';
-
 export interface Project {
   _id: string;
   title: string;
@@ -61,42 +55,6 @@ export interface Task {
   updatedAt: Date;
 }
 
-export type ProjectModel = mongoose.InferSchemaType<
-  (typeof ProjectModel)['schema']
-> & {
-  _id: string;
-  title: string;
-  description?: string;
-  owner: string;
-  members: string[];
-  tasks: Task[];
-  board: string;
-  createdAt: Date;
-  updatedAt: Date;
-};
-
-export type UserModel = mongoose.InferSchemaType<
-  (typeof UserModel)['schema']
-> & {
-  _id: string;
-  email: string;
-  name: string;
-};
-
-export type TaskModel = mongoose.InferSchemaType<
-  (typeof TaskModel)['schema']
-> & {
-  _id: string;
-  title: string;
-  description?: string;
-  dueDate?: Date;
-  board: string;
-  project: string;
-  assignee?: string;
-  creator: string;
-  lastModifier: string;
-};
-
 export interface Board {
   _id: string;
   title: string;
@@ -107,16 +65,3 @@ export interface Board {
   createdAt: Date;
   updatedAt: Date;
 }
-
-export type BoardModel = mongoose.InferSchemaType<
-  (typeof BoardModel)['schema']
-> & {
-  _id: string;
-  title: string;
-  description?: string;
-  owner: string;
-  members: string[];
-  projects: string[];
-  createdAt: Date;
-  updatedAt: Date;
-};
