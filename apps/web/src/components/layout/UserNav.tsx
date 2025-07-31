7244808f69d7c2af6f3a1d359e2fe487ddf14d83
JianAn Chen <john.data.chen@gmail.com>
2025-08-01 05:08:27 +0800

    fix: user avatar not displays and logout to incorrect url

--- apps/web/src/components/layout/UserNav.tsx
+++ apps/web/src/components/layout/UserNav.tsx
@@ -30,14 +30,16 @@ export function UserNav() {
           <Button variant="ghost" className="relative h-8 w-8 rounded-full">
             <Avatar className="h-8 w-8">
               {/* The new backend doesn't provide an image, so we use a fallback */}
-              <AvatarFallback>{user.name?.[0].toUpperCase()}</AvatarFallback>
+              <AvatarFallback>{user.email?.[0].toUpperCase()}</AvatarFallback>
             </Avatar>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="end" forceMount>
           <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
-              <p className="text-sm leading-none font-medium">{user.name}</p>
+              <p className="text-sm leading-none font-medium">
+                {user.email?.split('@')[0]}
+              </p>
               <p className="text-muted-foreground text-xs leading-none">
                 {user.email}
               </p>
