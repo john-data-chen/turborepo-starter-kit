557fc3f70403d9933fa8e4715738343a94ed2bce
JianAn Chen <john.data.chen@gmail.com>
2025-09-28 14:47:06 +0800

    refactor: getLocalePath

--- apps/web/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-export const getLocalePath = (path: string, locale: string | string[] | undefined) => {
-  const currentLocale = Array.isArray(locale) ? locale[0] : locale
-  if (currentLocale) {
-    return `/${currentLocale}${path}`
-  }
-  // Fallback to the path without locale if it's not present
-  return path
-}
