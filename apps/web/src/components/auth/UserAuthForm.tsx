cf84dae74879049bb34efa6a340d4d097e4c0015
JianAn Chen <john.data.chen@gmail.com>
2025-09-28 12:17:28 +0800

    fix: make input has more space for texts

--- apps/web/src/components/auth/UserAuthForm.tsx
+++ apps/web/src/components/auth/UserAuthForm.tsx
@@ -47,7 +47,7 @@ export default function UserAuthForm() {
                   placeholder={t('emailPlaceholder')}
                   disabled={isLoading || isNavigating}
                   data-testid="email-input"
-                  className="h-10 py-2 dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:placeholder-gray-400"
+                  className="h-10 py-2 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:placeholder-gray-400"
                   {...field}
                 />
               </FormControl>
