1912ecfa69043057ff9fa4d8b5a7786ac5dc4e31
JianAn Chen <john.data.chen@gmail.com>
2025-10-06 15:25:06 +0800

    test: add test commands in turborepo

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,17 +1,27 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    globals: true,
+    root: '.',
+    environment: 'jsdom',
     coverage: {
       reporter: ['text', 'json', 'html', 'lcov'],
-      include: ['apps/web/src/**/*.{ts,tsx}'],
+      include: ['./__tests__/units/**/*.test.{ts,tsx}'],
       exclude: [
-        'apps/web/src/**/*.d.ts',
-        'apps/web/__tests__/**/*.test.{ts,tsx}',
-        'apps/web/src/components/ui/**/*',
-        'apps/web/src/hooks/use-mobile.ts',
-        'apps/web/src/types/**/*'
+        'src/**/*.d.ts',
+        './__tests__/e2e/**/*.test.{ts,tsx}',
+        'src/components/ui/**/*',
+        'src/hooks/use-mobile.ts',
+        'src/types/**/*'
       ]
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@repo/ui': path.resolve(__dirname, '../../packages/ui/src')
     }
   }
 })
